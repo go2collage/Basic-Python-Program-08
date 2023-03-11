8ffd9de62aa65e009af2ab3c6e6328ad2abbd685
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-11 22:34:50 +0530

    Add files via upload

---
+++ BP_P8.py
@@ -0,0 +1,11 @@
+# Basic Python Program
+
+# Check Even or Odd number
+
+num = int(input("Enter a integer number: "))
+
+if num % 2 == 0:        # by using num % 2, check even number
+    print(num," is Even number.")
+else:                   # otherwise odd number
+    print(num," is Odd number.")
+
